80f93472b57043069f2d24671562ab807e8799f6
neuroevolutus <19356702+neuroevolutus@users.noreply.github.com>
2024-12-09 00:44:47 -0600

    Complete Advent of SQL 2024 Day 3 in PostgreSQL

---
+++ 2024/advent_of_sql_day_3_solution.sql
@@ -0,0 +1,57 @@
+WITH menu_one_food_ids AS (
+    SELECT
+        unnest(xpath('//*/food_item_id/text()', menu_data))::text food_ids
+    FROM
+        christmas_menus
+    WHERE (xpath('/*/@version', menu_data))[1]::text = '1.0'
+    AND (xpath('//total_count/text()', menu_data))[1]::text::integer > 78
+),
+menu_two_food_ids AS (
+    SELECT
+        unnest(xpath('//*/food_item_id/text()', menu_data))::text food_ids
+    FROM
+        christmas_menus
+    WHERE (xpath('/*/@version', menu_data))[1]::text = '2.0'
+    AND (xpath('//total_guests/text()', menu_data))[1]::text::integer > 78
+),
+menu_three_food_ids AS (
+    SELECT
+        unnest(xpath('//*/food_item_id/text()', menu_data))::text food_ids
+    FROM
+        christmas_menus
+    WHERE (xpath('/*/@version', menu_data))[1]::text = '3.0'
+    AND (xpath('//total_present/text()', menu_data))[1]::text::integer > 78
+),
+all_food_ids AS (
+    SELECT
+        *
+    FROM
+        menu_one_food_ids
+    UNION ALL
+    SELECT
+        *
+    FROM
+        menu_two_food_ids
+    UNION ALL
+    SELECT
+        *
+    FROM
+        menu_three_food_ids
+),
+food_id_counts AS (
+    SELECT
+        food_ids,
+        count(*) food_id_count
+    FROM
+        all_food_ids
+    GROUP BY
+        food_ids
+)
+SELECT
+    food_ids
+FROM
+    food_id_counts
+ORDER BY
+    food_id_count DESC
+LIMIT 1;
+
